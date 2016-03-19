29b17359c73d9a416ca4ad350f738a833d450f0f
Christopher gewecke <christophergewecke@gmail.com>
2016-03-19 12:16:08 -0700

    Issue #12 (Handle Profile data absence)

--- www/js/Controllers/profile.js
+++ www/js/Controllers/profile.js
@@ -13,5 +13,29 @@ function ProfileCtrl ($scope, GitHub){
   this.events = GitHub.events;
   this.viewTitle = GitHub.me.login;
   this.canFollow = false;
+
+  // Show order logic: There are four optional profile info fields
+  // and two spaces to show them in. In order of importance:
+  // 1. Company
+  // 2. Blog
+  // 3. Email
+  // 4. Location
+  this.company = this.user.company;
+  this.email = this.user.email;
+  this.blog = this.user.blog;
+  this.location = this.user.location;
+
+  // Case: both exist - no space
+  if (this.company && this.email){
+  
+  	this.blog = false;
+  	this.location = false;
+  
+  // Case: One exists - one space, pick blog, or location
+  } else if ( (!this.company && this.email) || (this.company && !this.email) ){
+
+	(this.blog) ? this.location = false : true;
+
+  } 
   
 };
