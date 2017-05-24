f050ce8c71ea2d8a097c2b8593cb08661b58a07d
c-g-e-w-e-k-e- <christophergewecke@gmail.com>
2017-05-23 20:20:41 -0700

    Update meteor-runtime-config.js

--- www/lib/meteor-client-side/meteor-runtime-config.js
+++ www/lib/meteor-client-side/meteor-runtime-config.js
@@ -2,8 +2,8 @@ __meteor_runtime_config__ = {};
 //__meteor_runtime_config__.DDP_DEFAULT_CONNECTION_URL = 'http://localhost:3000';
 
 // Development
-// Open SystemPreferences/Network to find the computed current IP address. It's not
-// necessarily 10.0.0.4 but will look something like that. Using this allows you to 
+// Open SystemPreferences/Network to find the computer's current IP address. It's probably not
+// 10.0.0.4 but will look something like that. This IP allows you to 
 // connect a mobile device to a locally run meteor server. Wifi must also be enabled on the 
 // computer. 
 // 
